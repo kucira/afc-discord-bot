6ad290717b8ee89c3bbc20cb789484ca7c8e202a
kucira <m4riberm4in@gmail.com>
2023-12-28 21:54:52 +0700

    feat: add callback button

--- modules/discord/views/intro_register.py
+++ modules/discord/views/intro_register.py
@@ -7,9 +7,12 @@ class IntroRegister(ui.View):
         super().__init__()
 
     @ui.button(label="Register Application", style=discord.ButtonStyle.green)
-    async def button1_callback(self, button, interaction):
+    async def register_btn_callback(self, interaction: discord.Interaction, button: ui.Button):
+        print("click register")
         await interaction.response.send_message("Register application clicked!")
 
     @ui.button(label="Browse Application", style=discord.ButtonStyle.secondary)
-    async def button2_callback(self, button, interaction):
+    async def browse_btn_callback(self, interaction: discord.Interaction, button: ui.Button):
+        print("click browse")
         await interaction.response.send_message("Browse application clicked!")
+
